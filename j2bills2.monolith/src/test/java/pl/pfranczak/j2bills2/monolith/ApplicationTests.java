104bbe91951224a68fc6b9e8548079a4cd18b229
Pawel Franczak <gzem89@gmail.com>
2020-10-08 21:50:52 +0200

    init

---
+++ j2bills2.monolith/src/test/java/pl/pfranczak/j2bills2/monolith/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pfranczak.j2bills2.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
