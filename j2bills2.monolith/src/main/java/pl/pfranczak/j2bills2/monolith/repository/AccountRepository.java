dfe5caf3d16f8ff6d4af218a62bb053b0638210f
Pawel Franczak <gzem89@gmail.com>
2020-10-13 10:32:33 +0200

    update repository arguments

--- j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/repository/AccountRepository.java
+++ j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/repository/AccountRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import pl.pfranczak.j2bills2.monolith.entity.Account;
+import pl.pfranczak.j2bills2.monolith.entity.User;
 
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long>{
