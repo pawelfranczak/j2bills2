5c935bc48415b1334662a0db6329414bb3f295b1
Pawel Franczak <gzem89@gmail.com>
2020-10-13 11:03:28 +0200

    update Journal Service

--- j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/service/JournalService.java
+++ j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/service/JournalService.java
@@ -1,15 +1,32 @@
 package pl.pfranczak.j2bills2.monolith.service;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
 
+import pl.pfranczak.j2bills2.monolith.entity.Account;
 import pl.pfranczak.j2bills2.monolith.entity.Journal;
 import pl.pfranczak.j2bills2.monolith.repository.JournalRepository;
 
 @Service
 public class JournalService extends CrudServiceImpl<Journal, Long>{
 
-	public JournalService(JournalRepository repository) {
+	private AccountService accountService;
+	
+	public JournalService(JournalRepository repository, AccountService accountService) {
 		super.setRepository(repository);
+		this.accountService = accountService;
 	}
+	
+	@Override
+	public void create(Journal entity) {
+		Account account = entity.getAccount();
+		BigDecimal oldBalance = account.getBalance();
+		account.setBalance(oldBalance.add(entity.getValue()));
+		entity.setBalanceOfAccountBeforeChange(oldBalance);
+		repository.save(entity);
+		accountService.update(account);
+	}
+
 	
 }
