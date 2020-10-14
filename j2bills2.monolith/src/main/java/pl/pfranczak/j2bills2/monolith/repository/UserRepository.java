dc617056a144b3c0e4de01b324f5bc7076b1d177
Pawel Franczak <gzem89@gmail.com>
2020-10-14 15:39:41 +0200

    show users which belongs only for owner

--- j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/repository/UserRepository.java
+++ j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/repository/UserRepository.java
@@ -1,11 +1,14 @@
 package pl.pfranczak.j2bills2.monolith.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import pl.pfranczak.j2bills2.monolith.authentication.UserAccount;
 import pl.pfranczak.j2bills2.monolith.entity.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
-	
+	List<User> findByOwner(UserAccount owner);
 }
--- j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/service/UserService.java
+++ j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/service/UserService.java
@@ -1,15 +1,37 @@
 package pl.pfranczak.j2bills2.monolith.service;
 
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import pl.pfranczak.j2bills2.monolith.authentication.UserAccount;
 import pl.pfranczak.j2bills2.monolith.entity.User;
 import pl.pfranczak.j2bills2.monolith.repository.UserRepository;
 
 @Service
 public class UserService extends CrudServiceImpl<User, Long>{
 
+	UserRepository userRepository;
+	
 	public UserService(UserRepository userRepository) {
 		super.setRepository(userRepository);
+		this.userRepository = userRepository;
+	}
+	
+	@Override
+	public List<User> getAll() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		// to do magic here, @PreAuthorise? need to retrive only data which belongs to logged user by id
+		UserAccount userAccount = (UserAccount) auth.getPrincipal();
+		List<User> allForLoggedUser = getAllForLoggedUser(userAccount);
+		return allForLoggedUser;
+	}
+	
+	private List<User> getAllForLoggedUser(UserAccount owner) {
+		return userRepository.findByOwner(owner);
 	}
 	
 }
