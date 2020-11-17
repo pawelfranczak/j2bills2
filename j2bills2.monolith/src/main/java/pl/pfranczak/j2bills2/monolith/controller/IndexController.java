2614b37393be70cd417c5e52e4f0792175c18bfb
Pawel Franczak <gzem89@gmail.com>
2020-11-17 09:52:02 +0100

    modify menu and index page

--- j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/controller/IndexController.java
+++ j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/controller/IndexController.java
@@ -2,13 +2,13 @@ package pl.pfranczak.j2bills2.monolith.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.AllArgsConstructor;
 import pl.pfranczak.j2bills2.monolith.service.AccountService;
-import pl.pfranczak.j2bills2.monolith.service.UserService;
 
 @AllArgsConstructor
 @Controller
@@ -19,12 +19,14 @@ public class IndexController {
 	@RequestMapping("${index}")
 	public ModelAndView index() {
 		
-		ModelAndView home = new ModelAndView("home");
-		home.addObject("sumOfAllAccounts", accountService.getSumOfAll());
-		
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (auth.isAuthenticated())
+		if (auth.isAuthenticated()) {
+			ModelAndView home = new ModelAndView("home");
+			home.addObject("sumOfAllAccounts", accountService.getSumOfAll());
+			UserDetails userDetails = (UserDetails) auth.getPrincipal();
+			home.addObject("userName", userDetails.getUsername());
 			return home;
+		}
 		return new ModelAndView("fail");
 	}
 	
