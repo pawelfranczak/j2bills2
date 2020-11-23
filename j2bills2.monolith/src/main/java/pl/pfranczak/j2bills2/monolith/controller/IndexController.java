094e43ebfe75ce6c27c1bab9c8f134a190a39330
Pawel Franczak <gzem89@gmail.com>
2020-11-23 15:31:36 +0100

    update

--- j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/controller/IndexController.java
+++ j2bills2.monolith/src/main/java/pl/pfranczak/j2bills2/monolith/controller/IndexController.java
@@ -2,7 +2,6 @@ package pl.pfranczak.j2bills2.monolith.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 import lombok.AllArgsConstructor;
 import pl.pfranczak.j2bills2.monolith.service.AccountService;
 import pl.pfranczak.j2bills2.monolith.service.UserService;
+import pl.pfranczak.j2bills2.monolith.service.UserSettingsService;
 
 @AllArgsConstructor
 @Controller
@@ -18,6 +18,8 @@ public class IndexController {
 	AccountService accountService;
 	
 	UserService userService;
+	
+	UserSettingsService userSettingsService;
 
 	@RequestMapping("${index}")
 	public ModelAndView index() {
@@ -25,7 +27,9 @@ public class IndexController {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		if (auth.isAuthenticated()) {
 			ModelAndView home = new ModelAndView("home");
-			home.addObject("sumOfAllAccounts", accountService.getSumOfAll());
+			if (userSettingsService.showAccountsSumOnHomepage()) {
+				home.addObject("sumOfAllAccounts", accountService.getSumOfAll());
+			}
 			userService.addUsernameToModelAndView(home);
 			return home;
 		}
